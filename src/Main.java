32198fbac9e3a5eb4c2a55b7541b0a8be350c7fd
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-09-14 20:07:49 +0300

    Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Strategy.ConcreteMathStrategyAdd;
+import Strategy.ConcreteMathStrategyMultiply;
+import Strategy.ConcreteMathStrategySubtract;
+import Strategy.StrategyMathContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Design Patterns");
+    }
+}
