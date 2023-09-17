8a0494158c3085abad2abe71355597374bc18609
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-09-17 23:09:07 +0300

    Implement an Example on Facade Pattern

---
+++ src/Facade/DemoFacade.java
@@ -0,0 +1,9 @@
+package Facade;
+import java.io.File;
+
+public class DemoFacade {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
